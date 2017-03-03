ce373c46babc9f5ba97ad4c6a5ab14dd9166d983
Scott Silliman <scott.t.silliman@gmail.com>
2017-03-03 04:17:37 -0500

    initial commit

---
+++ notebooks/config_notebook.py
@@ -0,0 +1,11 @@
+
+import os
+import sys
+
+def setup_pgh_ml_path():
+    curr_path = os.getcwd()
+    while not curr_path.endswith('PghML'):
+        curr_path = os.path.dirname(curr_path)
+    if curr_path.endswith('PghML'):
+        if curr_path not in sys.path:
+            sys.path.append(curr_path)
